8d65c9ff09c96ffff8d94a4048dacfa13b4e8c67
Qurat-ul-ain <ainie005@gmail.com>
2024-03-28 08:46:19 -0700

    push my bmi project to github

---
+++ main.js
@@ -0,0 +1,4 @@
+var weightInKgs = 68;
+var heightInMeters = 1.62;
+var Bmi = weightInKgs / (heightInMeters * heightInMeters);
+console.log("your BMI is = ".concat(Bmi));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInKgs = 68;
+let heightInMeters = 1.62;
+let Bmi = weightInKgs / (heightInMeters * heightInMeters)
+console.log(`your BMI is = ${Bmi}`);
